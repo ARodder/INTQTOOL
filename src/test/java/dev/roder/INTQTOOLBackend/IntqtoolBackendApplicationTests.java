865e3cedc1e26145061dd310af82baebac197fb0
Aleksander Roder <fylling8@gmail.com>
2022-02-20 17:01:47 +0100

    Initiated springboot project

---
+++ src/test/java/dev/roder/INTQTOOLBackend/IntqtoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.roder.INTQTOOLBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntqtoolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

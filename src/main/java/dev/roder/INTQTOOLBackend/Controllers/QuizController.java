387861031ec57f2cc841f987e858b0e3b764e941
Aleksander Roder <fylling8@gmail.com>
2022-04-08 20:34:36 +0200

    Added simple functionality of creating a new quiz.

--- src/main/java/dev/roder/INTQTOOLBackend/Controllers/QuizController.java
+++ src/main/java/dev/roder/INTQTOOLBackend/Controllers/QuizController.java
@@ -3,6 +3,8 @@ package dev.roder.INTQTOOLBackend.Controllers;
 import dev.roder.INTQTOOLBackend.Entities.Quiz;
 import dev.roder.INTQTOOLBackend.Services.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -31,6 +33,15 @@ public class QuizController {
     @PreAuthorize("hasRole('ROLE_STUDENT') or hasRole('ROLE_TEACHER') or hasRole('ROLE_ADMIN')")
     public @ResponseBody String getQuiz(@PathVariable("quizID") Integer quizID){
         return quizService.getQuiz(quizID);
+    }
+
+    @RequestMapping(method= RequestMethod.POST, path="/new")
+    @PreAuthorize("hasRole('ROLE_STUDENT') or hasRole('ROLE_TEACHER') or hasRole('ROLE_ADMIN')")
+    public @ResponseBody ResponseEntity<String> createQuiz(@RequestBody Quiz quiz){
+        ResponseEntity<String> response = new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+        quizService.addQuiz(quiz);
+
+        return response;
     }
 
 
--- src/main/java/dev/roder/INTQTOOLBackend/Services/QuizService.java
+++ src/main/java/dev/roder/INTQTOOLBackend/Services/QuizService.java
@@ -47,4 +47,8 @@ public class QuizService {
         }
 
     }
+
+    public void addQuiz(Quiz quiz){
+        quizRepository.save(quiz);
+    }
 }
